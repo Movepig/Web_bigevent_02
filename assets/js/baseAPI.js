a2a47e446ad186fa99ab1612d271b83e4da4c56f
xuebin <1787445928@qq.com>
2020-10-08 16:00:32 +0800

    成功完成主页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,12 +1,3 @@
-//注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
-// 会先调用 ajaxPrefilter 这个函数
-// 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function (params) {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    params.url = 'http://ajax.frontend.itheima.net' + params.url
-   
-})
-
 // //开发环境服务器地址
 // var baseURL = "http://ajax.frontend.itheima.net";
 // //2.测试环境服务器地址
@@ -17,3 +8,20 @@ $.ajaxPrefilter(function (params) {
 //     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
 //     params.url = baseURL + params.url
 // })
+
+
+//注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+// 会先调用 ajaxPrefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function (params) {
+    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+    params.url = 'http://ajax.frontend.itheima.net' + params.url
+   
+    //判断含有/my/开头的接口配置，可以使用这个函数的配置信息
+    if (params.url.indexOf("/my/") !== -1) { 
+        params.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+})
+
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+$(function () {
+    getUserInfo()
+    //点击退出
+    $('#btnLogout').on('click', function () {
+        // alert(1)
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            //1.删除本地存储token
+            localStorage.removeItem('token');
+            // 2.返回登录页面
+            location.href = "/login.html"
+            layer.close(index);
+        });
+    })
+})
+
+//设置为全局变量（写在如口函数外边），后边好调用
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization:localStorage.getItem("token") || ""
+        // },
+        success: function (res) {
+            // console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            renderAvatar(res.data)
+        },
+        //无论成功或者失败，都会触发complete方法
+        complete: function (res) {
+            console.log(res);
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                //1.删除本地token
+                localStorage.removeItem('token');
+                // 2.强制返回登录页面
+                location.href = "/login.html"
+            }
+        }
+
+    })
+}
+//封装用户头像渲染函数
+function renderAvatar(user) {
+    //用户名（昵称优先nickname（有头像），没有则使用首字母）
+    var name = user.nickname || user.username;
+    //渲染到页面上
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+    //用户头像
+    if (user.user_pic !== null) {
+        $(".layui-nav-img").show().attr("src", user.user_pic);
+        $(".user-avatar").hide()
+    } else {
+        $(".layui-nav-img").hide();
+        var text = name[0].toUpperCase();
+        $(".user-avatar").show().html(text)
+    }
+}
